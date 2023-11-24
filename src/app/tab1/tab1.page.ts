972d96f1e25245a7134f35bac7722113bf53b29a
Pedro Sanchez <psanchez@zafirus.tech>
2023-11-24 01:19:00 -0400

    agregar el ply

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Plyr from 'plyr';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor() {
+    const player = new Plyr('#player');
+  }
 
 }
