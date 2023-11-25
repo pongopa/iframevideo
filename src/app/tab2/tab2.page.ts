715cb14f9734178d5dc1e5f7ad85bd708b3ca60f
Pedro Sanchez <psanchez@zafirus.tech>
2023-11-24 22:49:00 -0400

    agregando nuevo metodo con object

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +10,17 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  public url: any;
+
+  constructor(public navCtrl: NavController, 
+              private sanitizer: DomSanitizer){
+    
+    this.getSafeUrl("https://www.youtube.com/embed/6k8cpUkKK4c?si=QNZY-x_sWGflUfea");
+  
+  }
+
+  getSafeUrl(ruta: string) {
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(ruta);    
+  }
 
 }
